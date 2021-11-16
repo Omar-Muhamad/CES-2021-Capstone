2759e4b198f1b0809c66f22862fda3c44878dc8f
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2021-11-16 09:36:09 +0200

    Finish hero section in both vesions

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // ################# Mobile Menu #################
 
-const menuBtn = document.getElementById('menuBtn');
-const closeBtn = document.getElementById('closeBtn');
-const mainNav = document.getElementById('mainNav');
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const mainNav = document.getElementById("mainNav");
 
-menuBtn.addEventListener('click', (event) => {
-    mainNav.classList.add('show')
+menuBtn.addEventListener("click", (event) => {
+  mainNav.classList.add("show");
 });
 
-mainNav.addEventListener('click', (event) => {
-    if (event.target.tagName === 'A' || event.target.tagName === 'I') {
-        mainNav.classList.remove('show')
-    }
+mainNav.addEventListener("click", (event) => {
+  if (event.target.tagName === "A" || event.target.tagName === "I") {
+    mainNav.classList.remove("show");
+  }
 });
+
