7f1ef9cf57d8bac7d68d491065527a7cc84dfa34
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2021-11-18 02:30:40 +0200

    Finish Home page and Change the content

--- script.js
+++ script.js
@@ -20,51 +20,51 @@ const data = [
   {
     speakerImageSrc: "./images/speaker_01.png",
     speakerImageAlt: "speaker 1 image",
-    speakerName: "Yohai Benkler",
-    speakerTitle: "Professor at Harvard Law School",
+    speakerName: "Lisa Su",
+    speakerTitle: "Business executive and electrical engineer, and CEO of AMD",
     speakerDesc:
-      "Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.",
+      "Early in her career, Su worked at Texas Instruments, IBM, and Freescale Semiconductor in engineering and management positions.",
   },
   {
     speakerImageSrc: "./images/speaker_02.png",
     speakerImageAlt: "speaker 2 image",
-    speakerName: "Jeon Gil-nam",
+    speakerName: "Pat Gelsinger",
     speakerTitle:
-      "Emeritus Professor, Korea Advanced Institute of Science and Technology (KAIST)",
+      "American business executive and engineer, currently serving as CEO of Intel",
     speakerDesc:
-      "By developing Asia's first Internet protocol network SDN and leading Korea's first private line Internet connection in 1990, it ushered in the era of the Internet in earnest.",
+      "Graduated from Stanford University with a master's degree in engineering. Previously, he was CEO of VMware and president and chief operating officer (COO) at EMC.",
   },
   {
     speakerImageSrc: "./images/speaker_03.png",
     speakerImageAlt: "speaker 3 image",
     speakerName: "Noh So-young",
-    speakerTitle: "Art Center Nabi Director, CC Korea Director",
+    speakerTitle: "Electrical engineer, and CEO of Qualcomm",
     speakerDesc:
-      "As the author of <Digital Art Art of Our Time>, he opened 'Art Center Nabi', Korea's first digital art institution in 2000, and is currently serving.",
+      "Amon began his Qualcomm career in 1995 as an engineer, and during his tenure, has helped shape the strategic direction for the Company in several leadership roles.",
   },
   {
     speakerImageSrc: "./images/speaker_04.png",
     speakerImageAlt: "speaker 4 image",
-    speakerName: "Julia Leda",
-    speakerTitle: "Head of the Young Pirates of Europe",
+    speakerName: "Koo Kwang-mo",
+    speakerTitle: "Korean Business man, and CEO of LG",
     speakerDesc:
-      "European integration and online youth participation in politics and democracy are major concerns, and a report has been published that will potentially affect the revision of the EU's",
+      "Young leader Koo Kwang-mo bore the burden of continuing this “right path” management and, at the same time, seeking survival in an ever more competitive global market.",
   },
   {
     speakerImageSrc: "./images/speaker_05.png",
     speakerImageAlt: "speaker 5 image",
-    speakerName: "Layla Tretikov",
-    speakerTitle: "Secretary General of the Wikimedia Foundation",
+    speakerName: "Kenichiro Yoshida",
+    speakerTitle: "Business executive, and CEO of Sony",
     speakerDesc:
-      "Layla Tretikov is the general secretary of the Wikimedia Foundation, a non-profit organization that runs Wikipedia. Wikipedia is provided free of charge in 290 languages ​​every month to over 100 million people, nearly half of the world's population.",
+      "Kenichiro Yoshida has been the Chief Financial Officer of Sony Corporation since April 1, 2014 and Executive Deputy President since April 2015. And served as an Executive Vice President of Sony since December 1, 2013.",
   },
   {
     speakerImageSrc: "./images/speaker_06.png",
     speakerImageAlt: "speaker 6 image",
-    speakerName: "Ryan Merkley",
-    speakerTitle: "Creative Commons CEO, Former Mozilla Foundation COO",
+    speakerName: "Koh Dong-jin",
+    speakerTitle: "Business executive, and CEO of Samsung",
     speakerDesc:
-      "He led open source projects at the Mozilla Foundation and joined CC as CEO in 2014. It has been actively promoting open movements such as open government and open source.",
+      "In 2015, Koh became the CEO of Samsung. In January 2020, Koh stepped down as Samsung CEO to become a co-CEO leading the company's IT and mobile communications division.",
   },
 ];
 
